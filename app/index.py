d87dfddc007a82ec4404a4591cd6aa2fd496d7b2
JuanCiroLo <jcirol@unal.edu.co>
2020-08-22 11:09:34 -0500

    organizado

--- app/index.py
+++ app/index.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     title = "Dynamic title"
-    return render_template('home.html', title=title)
-
+    return render_template("home.html", title=title)
 
 if __name__ == "__main__":
     app.run(debug=True)
